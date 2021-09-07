bdf7f904d71e00781c5232935721fcbac3e3bc49
Ubuntu <iivanov21@hotmail.com>
2021-09-07 11:33:52 +0000

    II:scripts

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,6 +3,8 @@ bash $WORKSPACE/scripts/docker.sh
 bash $WORKSPACE/scripts/docker-compose.sh
 sudo apt install nodejs
 sudo apt install npm
+sudo apt install junit4
+yes | sudo apt install default-jdk
 npm uninstall -g angular-cli @angular/cli
 yes | npm cache clean
 yes | npm install -g @angular/cli@latest
