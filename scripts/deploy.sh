b6a5efb36d6ba4d27e2d182ec87ee7198d494305
Ubuntu <admin10@jenkinsvm.hhxxzsxj1xmehopdbereauxuye.cwx.internal.cloudapp.net>
2021-09-09 17:54:55 +0000

    II:Deploy script fix

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 kubectl apply -f $WORKSPACE/backend/backend.yaml
+kubectl apply -f $WORKSPACE/frontend/configmap.yaml
+kubectl apply -f $WORKSPACE/frontend/loadbalancer.yaml
 kubectl apply -f $WORKSPACE/frontend/frontend.yaml
 kubectl get services
